bfa3fad99d9374cdfa6fd80165ed7b277d0feaa8
kurovvl <kurovvl@>
2024-05-20 18:36:06 +0300

    1st

---
+++ OtusTasks/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Parallel
+
+using OtusTasks;
+using System.Diagnostics;
+
+var stopwatch = Stopwatch.StartNew();
+var dir = "../../../";
+//var dir = "p:\\axes\\";
+
+var dct = SpaceFinder.GetCharCountByFile(dir, "*.txt");
+
+Console.WriteLine($"Elapsed time = {stopwatch.ElapsedMilliseconds} ms"); // таски
+
+stopwatch = Stopwatch.StartNew();
+var dct2 = SpaceFinder.GetCharCountByFileWithoutTask(dir, "*.txt");
+stopwatch.Stop();
+Console.WriteLine($"Elapsed time = {stopwatch.ElapsedMilliseconds} ms"); // без тасков
+
+Console.WriteLine("---");
+
+//foreach (var file in dct)
+//{
+//    Console.WriteLine(file);
+//}
+
+
+
+
+
---
+++ OtusTasks/SpaceFinder.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OtusTasks
+{
+    public static class SpaceFinder
+    {
+        public static IEnumerable<FileCharCnt> GetCharCountByFile(string directory, string pattern = null, char chr = ' ')
+        {
+            var FileNames = Directory.GetFiles(directory, pattern);
+            //var result = new ConcurrentDictionary<string, int>();
+            var TaskList = new List<Task<FileCharCnt>>();
+            Parallel.ForEach(FileNames, fn =>
+            {
+
+                //lock(result)
+                //{
+
+                var task = Task<FileCharCnt>.Factory.StartNew(() =>
+                {
+                    var origTxt = File.ReadAllText(fn);
+                    var replacedTxt = origTxt.Replace(chr.ToString(), "");
+
+                    return new FileCharCnt() 
+                    { 
+                        Filename = fn, 
+                        Count = origTxt.Length-replacedTxt.Length 
+                    };
+                });
+
+                TaskList.Add(task);
+            });
+
+            Task.WaitAll(TaskList.ToArray());
+            return TaskList.Select(s => s.Result);
+        }
+
+        public static ConcurrentDictionary<string, int> GetCharCountByFileWithoutTask(string directory, string pattern = null, char chr = ' ')
+        {
+            var FileNames = Directory.GetFiles(directory, pattern);
+            var result = new ConcurrentDictionary<string, int>();
+            
+            foreach (var fn in FileNames)
+            {
+                var cnt = File.ReadAllText(fn).Count(c => c == chr);
+                result.TryAdd(fn, cnt);
+            }
+            
+            return result;
+        }
+    }
+
+    public class FileCharCnt
+    {
+        public string Filename;
+        public int Count;
+    }
+}
